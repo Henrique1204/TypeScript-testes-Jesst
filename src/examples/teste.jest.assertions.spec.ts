b9d838d0706d0e5ba3d0a277f8340f72e79164af
Paulo <pauloh16.pdr@gmail.com>
2021-05-16 23:05:37 -0300

    Adicionar exemplos de assercoes com jest

---
+++ src/examples/teste.jest.assertions.spec.ts
@@ -0,0 +1,35 @@
+describe('Primitive values', () => {
+  it('should test jest assertions', () => {
+    const number = 10;
+
+    expect(number).toBe(10);
+    expect(number).toEqual(10);
+
+    expect(number).not.toBeNull();
+    expect(number).not.toBeFalsy();
+    expect(number).toBeTruthy();
+
+    expect(number).toBeGreaterThan(9);
+    expect(number).toBeGreaterThanOrEqual(10);
+    expect(number).toBeLessThan(11);
+    expect(number).toBeLessThanOrEqual(10);
+
+    expect(number).toBeCloseTo(10.001);
+
+    expect(number).toHaveProperty('toString');
+  });
+});
+
+describe('Objects', () => {
+  it('should teste jest asseritions with objects', () => {
+    const person = { name: 'Paulo', age: 19 };
+    const anotherPerson = { ...person };
+
+    expect(person).toEqual(anotherPerson);
+    expect(person).toHaveProperty('age');
+    expect(person).not.toHaveProperty('lastName');
+    expect(person).toHaveProperty('age', 19);
+
+    expect(person.name).toBe('Paulo');
+  });
+});
