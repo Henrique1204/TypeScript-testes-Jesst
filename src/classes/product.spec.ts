2b9c788bc7a7c51abaff5e75c270d5f1ccb08e4e
Paulo <pauloh16.pdr@gmail.com>
2021-05-18 02:02:59 -0300

    Adicionar teste da classe product

---
+++ src/classes/product.spec.ts
@@ -0,0 +1,13 @@
+import { Product } from './product';
+
+const createSut = (name: string, price: number): Product =>
+  new Product(name, price);
+
+describe('Product', () => {
+  it('should construct Product', () => {
+    const sut = createSut('Camisa', 59.0);
+
+    expect(sut).toHaveProperty('name', 'Camisa');
+    expect(sut.price).toBeCloseTo(59.0);
+  });
+});
